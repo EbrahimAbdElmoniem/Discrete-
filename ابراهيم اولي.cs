b5291db1278940363ccebcce97e6f4b32ae026aa
EbrahimAbdElmoniem <121164439+EbrahimAbdElmoniem@users.noreply.github.com>
2022-12-21 13:11:22 +0200

    Add files via upload

---
+++ ابراهيم اولي.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+
+class GFG
+{
+    public static void Main(string[] args)
+    {
+        //To run the program, click start without Debugging (Ctrl+F5)
+
+        int a, b, i, j, flag;
+
+        Console.WriteLine("Enter two numbers " +
+            "to find the prime numbers between them");
+
+
+        Console.WriteLine("Enter First number"); 
+                       
+       a = int.Parse(Console.ReadLine());
+
+       Console.WriteLine("\nEnter Second number");
+                       
+      b = int.Parse(Console.ReadLine());
+
+        
+        Console.WriteLine("\nPrime numbers between " +
+                        "{0} and {1} are: ", a, b);
+
+       
+        for (i = a; i <= b; i++)
+        {
+          
+            if (i == 1 || i == 0)
+                continue;
+           
+            flag = 1;
+
+            for (j = 2; j <= i / 2; ++j)
+            {
+                if (i % j == 0)
+                {
+                    flag = 0;
+                    break;
+                }
+            }
+        
+            if (flag == 1)
+                Console.WriteLine(i);
+
+            
+        }
+    }
+}
+
